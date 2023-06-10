f2402141aec592a0bd68e6fa94c87b5925abe275
kirito <yesiare.cn@gmail.com>
2023-06-10 14:44:59 +0800

    update

--- init.lua
+++ init.lua
@@ -6,6 +6,9 @@ set.clipboard = "unnamed" --剪切板和系统打通
 --set.autoindent = true
 set.splitbelow = true -- 控制默认的分屏方向
 set.splitright = true
+if vim.loop.os_uname().sysname == "Windows_NT" then
+	set.shellcmdflag = "--login -i -c"
+end
 
 require("autocmd")
 require("keymap").setup()
